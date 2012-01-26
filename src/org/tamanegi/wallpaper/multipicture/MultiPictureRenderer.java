2e821d059f2363383a7231b5221b0bf92467c0b1
lllllT <lllllT@tamanegi.org>
2012-01-26 21:57:51 +0900

    adjust workaround behaviour for some tablet

--- src/org/tamanegi/wallpaper/multipicture/MultiPictureRenderer.java
+++ src/org/tamanegi/wallpaper/multipicture/MultiPictureRenderer.java
@@ -1770,7 +1770,8 @@ public class MultiPictureRenderer
                 info.yoffset = (info.yoffset - 3f / 16f) * (16f / 10f);
             }
 
-            ycur_honeycomb = (info.ystep <= 0 ? 1 : info.yoffset / info.ystep);
+            ycur_honeycomb = ((info.ystep <= 0 || info.ystep == 1) ? 1 :
+                              info.yoffset / info.ystep);
 
             info.ystep = 0;
             info.yoffset = 0;
